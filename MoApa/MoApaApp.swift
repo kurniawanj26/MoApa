caac42000aef7e3317369f941c8ecda5a667f2d6
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2024-06-19 14:22:36 +0700

    Initial Commit

---
+++ MoApa/MoApaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoApaApp.swift
+//  MoApa
+//
+//  Created by Jayadi Kurniawan on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MoApaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
